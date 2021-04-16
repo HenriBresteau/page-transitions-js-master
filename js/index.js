dfa619de884c56370ac7973013095d97417e176e
Henri Brestea <henri.bresteau@gmail.com>
2021-04-16 15:49:51 +0200

    import Fade transition

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 const run = () => {
   console.log("index running");
 };
+
+import Highway from "@dogstudio/highway";
+import Fade from "./transition";
+
+const H = new Highway.Core({
+  transitions: {
+    default: Fade,
+  },
+});
